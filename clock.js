ac7a7ee9293752d64013ec56f0c61d5ff57cccb3
TakeALook-KR <67315431+TakeALook-KR@users.noreply.github.com>
2020-07-14 14:35:37 +0900

    Add files via upload

--- clock.js
+++ clock.js
@@ -5,9 +5,8 @@ function getTime() {
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
     clockTitle.innerText = `TakeALook's WEB에 오신걸 환영합니다! 현재 시간은 ${hours < 10 ? `0${hours}` : hours}:${
-        minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds} 입니다.`;
+        minutes < 10 ? `0${minutes}` : minutes} 입니다.`;
 }
 
 function init() {
